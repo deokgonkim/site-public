314f626c6d38a854b13ffdaaeb4df38b064415d4
Deokgon Kim <dgkim@dgkim.net>
2024-11-27 18:17:19 +0900

    feat: support shop description display

--- apps/guest/src/pages/first.jsx
+++ apps/guest/src/pages/first.jsx
@@ -2,6 +2,9 @@ import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
 import {
   Button,
+  Card,
+  CardContent,
+  CardHeader,
   Container,
   FormControl,
   InputAdornment,
@@ -12,6 +15,7 @@ import {
 } from "@mui/material";
 import { TextField, Box } from "@mui/material";
 import { useEffect, useState } from "react";
+import Markdown from "react-markdown";
 
 import guestApi from "../api/guestApi";
 import { basePath } from "../config";
@@ -74,97 +78,118 @@ const FirstPage = () => {
         <Typography variant="h4">
           {shopUid} - {shop?.shopName}
         </Typography>
-        <Typography variant="h4">
-          <FormattedMessage id="first.title" />
-        </Typography>
-        <Box
-          component="form"
-          sx={{
-            padding: "1em",
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            alignContent: "center",
-          }}
-        >
-          <TextField
-            label={intl.formatMessage({ id: "field.name" })}
-            value={customerName}
-            autoComplete="name"
-            onChange={(e) => setCustomerName(e.target.value)}
-            required
+        <Card style={{ marginTop: "1em" }}>
+          <CardHeader
+            title={intl.formatMessage(
+              {
+                id: "shop.description.header",
+              },
+              {
+                name: shop?.shopName,
+              }
+            )}
           />
-          <FormControl variant="outlined">
-            <InputLabel id="country-code-label">
-              {intl.formatMessage({ id: "field.countryCode" })}
-            </InputLabel>
-            <Select
-              label={intl.formatMessage({ id: "field.countryCode" })}
-              labelId="country-code-label"
-              id="country-code"
-              value={countryCode}
-              onChange={(e) => {
-                setCountryCode(e.target.value);
-                setCustomerPhone("");
+          <CardContent>
+            <Markdown>{shop?.description}</Markdown>
+          </CardContent>
+        </Card>
+        <Card style={{ marginTop: "1em" }}>
+          <CardContent>
+            <Typography variant="h4">
+              <FormattedMessage id="first.title" />
+            </Typography>
+            <Box
+              component="form"
+              sx={{
+                padding: "1em",
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+                alignContent: "center",
               }}
             >
-              <MenuItem value="+82">🇰🇷 +82 (South Korea)</MenuItem>
-              <MenuItem value="+1">🇺🇸 +1 (USA)</MenuItem>
-              <MenuItem value="+44">🇬🇧 +44 (UK)</MenuItem>
-              <MenuItem value="+49">🇩🇪 +49 (Germany)</MenuItem>
-              <MenuItem value="">
-                {intl.formatMessage({ id: "other" })}
-              </MenuItem>
-              {/* Add more countries as needed */}
-            </Select>
-          </FormControl>
-          {/* 뭔가 마음에 안드는 전화번호 핸들링이다. */}
-          {/* iOS Safari에서 countryCode가 포함된 번호 Auto fill 할 때, */}
-          {/* 국가코드가 날라가서, 국가코드 컬럼이 들어가고, 값에 대한 조작이 발생하고 있다. */}
-          <TextField
-            label={intl.formatMessage({ id: "field.phone" })}
-            name="phone"
-            autoComplete="mobile tel"
-            type="tel"
-            value={
-              customerPhone.startsWith(countryCode)
-                ? customerPhone.slice(countryCode.length)
-                : customerPhone
-            }
-            onChange={(e) => {
-              if (
-                e.target.value.match(/^\+\d/) &&
-                !e.target.value.startsWith(countryCode)
-              ) {
-                setCustomerPhone("");
-                return;
-              }
-              const cleanNumber = e.target.value.startsWith("+")
-                ? "+" + e.target.value.replace(/\D/g, "")
-                : e.target.value.replace(/\D/g, "");
-              setCustomerPhone(
-                e.target.value.startsWith(countryCode)
-                  ? cleanNumber
-                  : countryCode + cleanNumber
-              );
-            }}
-            required
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">{countryCode}</InputAdornment>
-              ),
-            }}
-          />
-          <TextField
-            label={intl.formatMessage({ id: "field.orderItem" })}
-            value={orderItem}
-            onChange={(e) => setOrderItem(e.target.value)}
-            required
-          />
-          <Button onClick={placeOrder}>
-            <FormattedMessage id="first.placeOrder" />
-          </Button>
-        </Box>
+              <TextField
+                label={intl.formatMessage({ id: "field.name" })}
+                value={customerName}
+                autoComplete="name"
+                onChange={(e) => setCustomerName(e.target.value)}
+                required
+              />
+              <FormControl variant="outlined">
+                <InputLabel id="country-code-label">
+                  {intl.formatMessage({ id: "field.countryCode" })}
+                </InputLabel>
+                <Select
+                  label={intl.formatMessage({ id: "field.countryCode" })}
+                  labelId="country-code-label"
+                  id="country-code"
+                  value={countryCode}
+                  onChange={(e) => {
+                    setCountryCode(e.target.value);
+                    setCustomerPhone("");
+                  }}
+                >
+                  <MenuItem value="+82">🇰🇷 +82 (South Korea)</MenuItem>
+                  <MenuItem value="+1">🇺🇸 +1 (USA)</MenuItem>
+                  <MenuItem value="+44">🇬🇧 +44 (UK)</MenuItem>
+                  <MenuItem value="+49">🇩🇪 +49 (Germany)</MenuItem>
+                  <MenuItem value="">
+                    {intl.formatMessage({ id: "other" })}
+                  </MenuItem>
+                  {/* Add more countries as needed */}
+                </Select>
+              </FormControl>
+              {/* 뭔가 마음에 안드는 전화번호 핸들링이다. */}
+              {/* iOS Safari에서 countryCode가 포함된 번호 Auto fill 할 때, */}
+              {/* 국가코드가 날라가서, 국가코드 컬럼이 들어가고, 값에 대한 조작이 발생하고 있다. */}
+              <TextField
+                label={intl.formatMessage({ id: "field.phone" })}
+                name="phone"
+                autoComplete="mobile tel"
+                type="tel"
+                value={
+                  customerPhone.startsWith(countryCode)
+                    ? customerPhone.slice(countryCode.length)
+                    : customerPhone
+                }
+                onChange={(e) => {
+                  if (
+                    e.target.value.match(/^\+\d/) &&
+                    !e.target.value.startsWith(countryCode)
+                  ) {
+                    setCustomerPhone("");
+                    return;
+                  }
+                  const cleanNumber = e.target.value.startsWith("+")
+                    ? "+" + e.target.value.replace(/\D/g, "")
+                    : e.target.value.replace(/\D/g, "");
+                  setCustomerPhone(
+                    e.target.value.startsWith(countryCode)
+                      ? cleanNumber
+                      : countryCode + cleanNumber
+                  );
+                }}
+                required
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      {countryCode}
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                label={intl.formatMessage({ id: "field.orderItem" })}
+                value={orderItem}
+                onChange={(e) => setOrderItem(e.target.value)}
+                required
+              />
+              <Button onClick={placeOrder}>
+                <FormattedMessage id="first.placeOrder" />
+              </Button>
+            </Box>
+          </CardContent>
+        </Card>
       </Container>
     </>
   );
