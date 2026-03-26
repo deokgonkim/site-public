caa287b7fd256a2cdbb186efb39731a10b8218c8
Deokgon Kim <dgkim@dgkim.net>
2026-03-26 14:26:17 +0900

    update quartz/mac-software/quartz.config.tx

--- quartz/mac-software/quartz.config.ts
+++ quartz/mac-software/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Mac Softwares",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
